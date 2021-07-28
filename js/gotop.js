507b1308ed12fe9164f9aa50574769041034b3ea
sousuke0422 <sousuke0422@users.noreply.github.com>
2021-07-28 08:13:20 +0000

    deploy: 8276e6c29ba38800ddeb3cc716daaba1612d0e72

--- js/gotop.js
+++ js/gotop.js
@@ -1,3 +1,3 @@
-// build time:Fri May 21 2021 09:09:17 GMT+0000 (Coordinated Universal Time)
+// build time:Wed Jul 28 2021 08:13:19 GMT+0000 (Coordinated Universal Time)
 (function(i){jQuery.fn.gotoTop=function(o){var n=this;var a=i(window);var e=i("html,body");var t=false;var s={offset:420,speed:500,iconSpeed:200,animationShow:{opacity:"1"},animationHide:{opacity:"0"}};var c=i.extend(s,o);n.click(function(){e.animate({scrollTop:"0"},c.speed)});i.each(c.animationShow,function(i){if(i=="transform"){t=true}});function f(){if(t){n.css(c.animationShow)}else{n.stop().animate(c.animationShow,c.iconSpeed)}}function l(){if(t){n.css(c.animationHide)}else{n.stop().animate(c.animationHide,c.iconSpeed)}}a.scroll(function(){if(a.scrollTop()>c.offset){f()}else{l()}});if(a.scrollTop()>c.offset){n.css(c.animationShow)}else{n.css(c.animationHide)}}})(jQuery);
 //rebuild by hrmmi 
