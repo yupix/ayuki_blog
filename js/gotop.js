514713a5c742b4bdeaeeae1dfb8b72f2ba90787e
yupix <yupix@users.noreply.github.com>
2021-08-28 18:51:02 +0000

    deploy: 39773182891eae5830ed3398683a0f173d034408

--- js/gotop.js
+++ js/gotop.js
@@ -1,3 +1,3 @@
-// build time:Sat Aug 28 2021 18:30:12 GMT+0000 (Coordinated Universal Time)
+// build time:Sat Aug 28 2021 18:51:01 GMT+0000 (Coordinated Universal Time)
 (function(i){jQuery.fn.gotoTop=function(o){var n=this;var a=i(window);var e=i("html,body");var t=false;var s={offset:420,speed:500,iconSpeed:200,animationShow:{opacity:"1"},animationHide:{opacity:"0"}};var c=i.extend(s,o);n.click(function(){e.animate({scrollTop:"0"},c.speed)});i.each(c.animationShow,function(i){if(i=="transform"){t=true}});function f(){if(t){n.css(c.animationShow)}else{n.stop().animate(c.animationShow,c.iconSpeed)}}function l(){if(t){n.css(c.animationHide)}else{n.stop().animate(c.animationHide,c.iconSpeed)}}a.scroll(function(){if(a.scrollTop()>c.offset){f()}else{l()}});if(a.scrollTop()>c.offset){n.css(c.animationShow)}else{n.css(c.animationHide)}}})(jQuery);
 //rebuild by hrmmi 
